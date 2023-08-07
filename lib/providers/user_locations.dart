52ded7245ab24569f58a475450ef8b1a6be01eb9
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-06 22:25:50 -0400

    changes

--- lib/providers/user_locations.dart
+++ lib/providers/user_locations.dart
@@ -1,11 +1,22 @@
 import 'dart:io';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wanderlust_favourites/models/location.dart';
 
-/// A class that manages a list of user locations.
+/// A class that manages a list of user locations and allows adding new locations to the list.
 ///
-/// This class allows adding new locations to the list.
+/// This class extends the 'StateNotifier' class and manages the state of the list of locations.
+/// The state is represented by a list of 'Location' objects.
+///
+/// To add a new location, use the 'addLocation' method and provide the title and image file of the location.
+/// The method creates a new 'Location' object with the provided title and image, and adds it to the beginning of the list.
+///
+/// Example usage:
+/// ```dart
+/// final userLocations = useProvider(userLocationProvider);
+///
+/// userLocations.addLocation('New York', File('new_york.jpg'));
+/// ```
+
 class UserLocationsNotifier extends StateNotifier<List<Location>> {
   UserLocationsNotifier() : super(const []);
 
@@ -14,7 +25,6 @@ class UserLocationsNotifier extends StateNotifier<List<Location>> {
     state = [newLocation, ...state];
   }
 }
-//<UserLocationsNotifier, List> telling the provider the kind of data that would be managed
 
 final userLocationProvider =
     StateNotifierProvider<UserLocationsNotifier, List<Location>>(
--- lib/screens/add_location.dart
+++ lib/screens/add_location.dart
@@ -3,9 +3,19 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wanderlust_favourites/providers/user_locations.dart';
 import 'package:wanderlust_favourites/widgets/image_input.dart';
+// This code snippet defines a Flutter screen for adding a new location. It includes a text field for the location title and an image picker widget for selecting an image. The screen uses Flutter Riverpod for state management.
+
+// The AddLocationScreen class extends ConsumerStatefulWidget, which allows it to access and update state using the Riverpod library. It has a single ConsumerState<AddLocationScreen> as its state class.
+
+// The state class, _AddLocationScreenState, manages the state of the screen. It includes a TextEditingController for the title field and a File variable for storing the selected image. The _saveLocation method is called when the user taps the save button. It checks if the title and image are non-empty and then adds the location to the user's list of locations using the userLocationProvider. Finally, it closes the screen and returns to the previous screen.
+
+// The build method constructs the UI of the screen using Flutter's widget tree. It includes a Scaffold widget with an AppBar and a body that contains a SingleChildScrollView. Inside the SingleChildScrollView, there is a Column widget with a TextField for the title, an ImageInput widget for selecting an image, and an ElevatedButton for saving the location.
+
+// The dispose method is overridden to clean up resources when the screen is closed.
+
 
 class AddLocationScreen extends ConsumerStatefulWidget {
-  const AddLocationScreen({super.key});
+  const AddLocationScreen({Key? key}) : super(key: key);
 
   @override
   ConsumerState<AddLocationScreen> createState() => _AddLocationScreenState();
