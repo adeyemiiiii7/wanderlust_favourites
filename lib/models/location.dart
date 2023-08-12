ae49ed6dcf7a4d38fb4f2451ae5905bf55dc03c3
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-11 22:29:44 -0400

    get location

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -16,12 +16,15 @@ class PlaceLocation {
   final String address;
 }
 
-class Location {
-  Location({required this.title, required this.image, required this.location})
-      : id = uuid.v4();
+class Locations {
+  Locations({
+    required this.title,
+    required this.image,
+    // required this.locations,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final File image;
-  final PlaceLocation location;
+  // final PlaceLocation locations;
 }
