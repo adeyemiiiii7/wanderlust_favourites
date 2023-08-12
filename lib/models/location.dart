c1f7b94296037bbf9509bcbe35594375288c4224
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-11 22:29:06 -0400

    get location

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -4,21 +4,24 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
-class PlaceLocation{
+class PlaceLocation {
   const PlaceLocation({
     required this.latitude,
-    required this.longitude
-    required this.address
+    required this.longitude,
+    required this.address,
   });
 
-final double latitude;
-final double longitude;
-final String address;
+  final double latitude;
+  final double longitude;
+  final String address;
 }
+
 class Location {
-  Location({required this.title, required this.image}) : id = uuid.v4();
+  Location({required this.title, required this.image, required this.location})
+      : id = uuid.v4();
 
   final String id;
   final String title;
   final File image;
+  final PlaceLocation location;
 }
